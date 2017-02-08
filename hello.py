bcb61c13b5520edd70fbc2d5a188ce320948ed52
vinodchowl <chowlvinod@gmail.com>
2017-02-08 17:18:37 +0530

    added the thirdline

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 def helloworld():
     print ('The first line adding to practice git and python as well.')
     print ('Welcome to the python world')
+    print ('Now added the third line to check out with the path concept.')
 helloworld()
