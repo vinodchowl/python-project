1e0aca695fd2a0b3457ea181cb622c2400c366f4
vinodchowl <chowlvinod@gmail.com>
2017-02-08 11:26:49 +0530

    added function to the hello program

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
-print ('The first line adding to practice git and python as well.')
-print ('Welcome to the python world')
+def helloworld():
+    print ('The first line adding to practice git and python as well.')
+    print ('Welcome to the python world')
+helloworld()
