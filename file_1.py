1246dba757fa098a97b5a089746e1d6f563c8b96
vinodchowl <chowlvinod@gmail.com>
2017-02-08 14:50:08 +0530

    changed \r with \n

--- file_1.py
+++ file_1.py
@@ -11,7 +11,7 @@ try:
     
     with open(sys.argv[1], 'r', encoding='utf-8-sig') as fh1, open('swapped_'+os.path.basename(sys.argv[1]), 'w+', newline='\r\n') as fh2:
         for line in fh1:
-            fh2.write(swapnibbles(line)+'\r')
+            fh2.write(swapnibbles(line)+'\n')
 except Exception as e:
     print ('Below Exception is raised, program excecution failed.')
     print (e)
