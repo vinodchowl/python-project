657401a84fcc64b36c39821b7cd6f48af17449d8
vinodchowl <chowlvinod@gmail.com>
2017-02-08 14:39:42 +0530

    removed the extras.

--- file_1.py
+++ file_1.py
@@ -12,7 +12,6 @@ try:
     with open(sys.argv[1], 'r', encoding='utf-8-sig') as fh1, open('swapped_'+os.path.basename(sys.argv[1]), 'w+', newline='\r\n') as fh2:
         for line in fh1:
             fh2.write(swapnibbles(line)+'\r')
-
 except Exception as e:
     print ('Below Exception is raised, program excecution failed.')
     print (e)
