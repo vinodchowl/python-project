6756b5e0c9bdfa491da6e551e6b49622874f7cf3
vinodchowl <chowlvinod@gmail.com>
2017-02-14 15:46:31 +0530

    added string reverse in c file.

--- string_count.c
+++ string_count.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int string_count (char str[]) {
 	int i;
@@ -17,10 +19,20 @@ int string_length (char *str) {
 	return count;
 }
 
+char *string_reverse (char *str) {
+	int len = strlen(str);
+	char *temp = (char *)malloc(len);
+	int i,j;
+	for (i = len-1, j = 0; i >= 0; i--, j++) {
+		temp[j] = str[i];
+	}
+	temp[j] = '\0';
+	return temp;
+}
 void main() {
 	char str[100]="\0";
 	printf ("Enter the string: ");
 	scanf ("%[^\n]", &str);
 	printf ("The enterd string count is: %d\n",string_count(str));
-	printf ("The enterd string count is: %d\n",string_length(str));
+	printf ("The enterd string reverse is: %s\n",string_reverse(str));
 }
