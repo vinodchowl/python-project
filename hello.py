05b70bb51515b0d62ad0a9541670a2be43383e1e
vinodchowl <chowlvinod@gmail.com>
2017-03-17 10:47:15 +0530

    added fifth line without updating the repo

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ def helloworld():
     print ('The first line adding to practice git and python as well.')
     print ('Welcome to the python world')
     print ('Now added the third line to check out with the path concept.')
+    print ('Now added the fifth line to check out with the path concept.')
 helloworld()
